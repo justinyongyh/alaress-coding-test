4c2f0a0ed83327ed6c46f55c64de1f32c14dd713
Justin <justinyong.yh@gmail.com>
2019-03-16 01:00:03 +1100

    First attempt on coding test

---
+++ homepage.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8"/>
+    <link rel="icon" type="image/png" href="assets/img/favicon.ico">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+    <title>Coding Test Dashboard</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
+    <meta name="viewport" content="width=device-width"/>
+
+    <!-- External CSS Link -->
+    <link href="homepage.css" rel="stylesheet" type="text/css"/>
+
+</head>
+
+
+<body>
+<div>
+
+    <div>
+     
+        <img class="logo" src="images/school-box-logo-white.png">
+        <img class="header-image" src="images/hands-raised-sb-blue-cropped.jpg" height="350" width="100%">
+
+        <div class="overlay">
+        </div>
+
+
+    </div>
+
+
+
+
+</div>
+</body>
+
