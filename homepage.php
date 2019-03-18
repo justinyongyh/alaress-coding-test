78de032216a9c924490f92b15552a91294c44f70
Justin <justinyong.yh@gmail.com>
2019-03-19 01:39:02 +1100

    Tidy up code

--- homepage.php
+++ homepage.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8"/>
     <link rel="icon" type="image/png" href="assets/img/favicon.ico">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
-    <title>Coding Test Dashboard</title>
+    <title>Schoolbox Coding Test</title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport'/>
     <meta name="viewport" content="width=device-width"/>
 
@@ -19,17 +19,17 @@
 
     <div class="header-image">
      
+        <!-- Schoolbox Logo -->
         <img class="logo" src="images/school-box-logo-white.png">
 
-        <div class="overlay">
+        <div class="header-overlay">
         </div>
 
-
-        <div style="text-align: center;">
-            <table border="0" class="header-table" align="center">
+        <div class="align-center">
+            <table border="0" class="header-table">
                 <tr height="20">
                     <td>
-                        <div class="header-1" style="color: white;">
+                        <div class="header-1">
                             WHITEPAPER
                         </div>
                     </td>
@@ -37,7 +37,7 @@
 
                 <tr height="50">
                     <td>
-                        <div class="header-2" style="color: white;">
+                        <div class="header-2">
                             Woohoo!
                         </div>
                     </td>
@@ -45,7 +45,7 @@
 
                 <tr height="70">
                     <td>
-                        <div class="header-3" style="color: white;">
+                        <div class="header-3">
                             Must-Have School Portal Features to Drive Community Engagement
                         </div>
                     </td>
@@ -53,7 +53,7 @@
 
                 <tr height="70">
                     <td>
-                        <div class="header-4" style="color: white;">
+                        <div class="header-4">
                             <ul>
                                 <li>
                                     Smartphones as learning tools
@@ -69,19 +69,20 @@
 
             </table>
 
+            <!-- Form table -->
             <table border="0" bgcolor="white" class="header-table-2" align="center" style="padding: 12px 20px;">
 
                 <tr>
                     <td>
-                        <div class="form-main-header" style="color: #0066B0;">
+                        <div class="form-main-header">
                             Fill in the form and we’ll email you the download link
                         </div>
                     </td>
                 </tr>
 
                 <tr>
-                    <td style="padding-top: 8px;">
-                        <div class="form-question" style="color: #0066B0;">
+                    <td class="padding-top-8">
+                        <div class="form-question">
                             FIRST NAME
                         </div>
                     </td>
@@ -89,13 +90,13 @@
 
                 <tr>
                     <td>
-                        <input type="text" name="firstname" size="14" style="font-size: 20px;">
+                        <input class="font-20" type="text" name="firstname" size="14">
                     </td>
                 </tr>
 
                 <tr>
-                    <td style="padding-top: 8px;">
-                        <div class="form-question" style="color: #0066B0;">
+                    <td class="padding-top-8">
+                        <div class="form-question">
                             LAST NAME YOU REPRESENT
                         </div>
                     </td>
@@ -103,13 +104,13 @@
 
                 <tr>
                     <td>
-                        <input type="text" name="lastname" size="14" style="font-size: 20px;">
+                        <input class="font-20" type="text" name="lastname" size="14">
                     </td>
                 </tr>
 
                 <tr>
-                    <td style="padding-top: 8px;">
-                        <div class="form-question" style="color: #0066B0;">
+                    <td class="padding-top-8">
+                        <div class="form-question">
                             EMAIL
                         </div>
                     </td>
@@ -117,13 +118,13 @@
 
                 <tr>
                     <td>
-                        <input type="text" name="email" size="14" style="font-size: 20px;">
+                        <input class="font-20" type="text" name="email" size="14" >
                     </td>
                 </tr>
 
                 <tr>
-                    <td style="padding-top: 8px;">
-                        <div class="form-question" style="color: #0066B0;">
+                    <td class="padding-top-8">
+                        <div class="form-question">
                             CURRENT SIS
                         </div>
                     </td>
@@ -131,13 +132,13 @@
 
                 <tr>
                     <td>
-                        <input type="text" name="current" size="14" style="font-size: 20px;">
+                        <input class="font-20" type="text" name="current" size="14">
                     </td>
                 </tr>
 
                 <tr>
-                    <td style="padding-top: 20px;">
-                        <button class="button-style">
+                    <td class="padding-top-20">
+                        <button class="get-your-copy-button-style">
                             GET YOUR COPY NOW
                         </button>
                     </td>
@@ -151,32 +152,32 @@
     <div>
         <table border="0" class="section-two-table" align="center">
             <tr>
-                <td style="padding-top: 14px;">
-                    <div class="section-two-first-paragraph" style="color: #0066B0;">
+                <td class="padding-top-14">
+                    <div class="section-two-first-paragraph">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec mattis arcu. Nulla facilisi. Suspendisse venenatis maximus lorem, a iaculis mauris maximus sed.
                     </div>
                 </td>
             </tr>
 
             <tr>
-                <td style="padding-top: 25px;">
-                    <div class="section-two-second-paragraph" style="color: #0066B0;">
+                <td class="padding-top-25">
+                    <div class="section-two-second-paragraph">
                         Maecenas non lorem venenatis, rutrum purus ut, euismod massa. Sed pretium, ante at eleifend blandit, arcu orci accumsan lacus, ac tristique tortor velit a nisi. Nam non ligula vitae arcu dictum posuere at nec augue. Etiam eget risus quam. Proin eget sapien velit. Ut pretium mollis nunc, id eleifend nulla semper vestibulum. Praesent finibus ipsum et massa convallis fermentum. Vestibulum at blandit lorem.
                     </div>
                 </td>
             </tr>
 
             <tr>
-                <td style="padding-top: 20px;">
-                    <div class="section-two-third-paragraph" style="color: #0066B0;">
+                <td class="padding-top-20">
+                    <div class="section-two-third-paragraph">
                         Heading 3
                     </div>
                 </td>
             </tr>
 
             <tr>
-                <td style="padding-top: 20px;">
-                    <div class="section-two-second-paragraph" style="color: #0066B0;">
+                <td class="padding-top-20">
+                    <div class="section-two-second-paragraph">
                         Porttitor sollicitudin neque. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras facilisis pharetra commodo. Fusce non arcu ac nisi elementum ullamcorper ut sit amet ligula. Curabitur euismod odio quis ante sodales, vitae egestas ex pellentesque. Sed pulvinar blandit feugiat. Nulla facilisi. Duis molestie consectetur aliquet. Curabitur egestas vestibulum sapien, vel vestibulum mi ultrices id. Nulla ultricies
                     </div>
                 </td>
@@ -186,6 +187,40 @@
     </div>
 
     <div class="footer-image">
+
+        <div class="footer-overlay">
+        </div>
+
+        <div class="align-center">
+
+            <table border="0" class="footer-table" align="center">
+                <tr>
+                    <td colspan="2" class="padding-bottom-60">
+                        <div class="footer-message">
+                            Want amazing education news, tips ‘n tricks in your inbox? Then sign up today!
+                        </div>
+                    </td>
+                </tr>
+
+                <tr>
+                    <td>
+                        <div class="footer-email">
+                            EMAIL
+                        </div>
+
+                        <div class="float-left">
+                            <input class="font-20" type="text" name="footer-email" size="18">
+                        </div>
+                    </td>
+
+                    <td class="padding-top-16">
+                        <button class="sign-up-button-style">SIGN UP</button>
+                    </td>
+                </tr>
+            </table>
+
+        </div>
+
     </div>
 
 
